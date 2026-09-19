b3d35c72b766750ea06fd159e68ca2274400c8a6
drewbabel <122849144+drewbabel@users.noreply.github.com>
2026-09-19 12:50:49 -0400

    [rtl] Return element zero to an x register

--- rtl/datapath.sv
+++ rtl/datapath.sv
@@ -43,8 +43,7 @@ module datapath
     output logic [     6:0] dbg_vstart,
     output logic [     7:0] dbg_vec_tag,
     output logic            dbg_vec_retire,
-    output logic [     4:0] dbg_vec_vd,
-    output logic [     3:0] dbg_vec_regs,
+    output logic [    31:0] dbg_vec_wregs,
     output logic            dbg_vec_idle,
     output logic            dbg_ex_commit,
     output logic [XLEN-1:0] dbg_ex_insn,
@@ -220,6 +219,8 @@ module datapath
   logic                                  vec_load_pending;
   logic                                  vec_store_pending;
   logic                                  vec_misaligned;
+  logic                                  vec_xreg_valid;
+  logic                   [    XLEN-1:0] vec_xreg_result;
   logic                                  vmem_hold;
   logic                                  is_fence_ex;
   logic                   [    XLEN-1:0] vec_bad_addr;
@@ -646,8 +647,7 @@ module datapath
 `ifdef RISCV_FORMAL
       .dbg_vec_tag   (dbg_vec_tag),
       .dbg_vec_retire(dbg_vec_retire),
-      .dbg_vec_vd    (dbg_vec_vd),
-      .dbg_vec_regs  (dbg_vec_regs),
+      .dbg_vec_wregs (dbg_vec_wregs),
       .dbg_vec_idle  (dbg_vec_idle),
 `endif
       .clk        (clk),
@@ -671,6 +671,8 @@ module datapath
       .mem_wstrb  (vmem_wstrb),
       .mem_misaligned(vec_misaligned),
       .mem_bad_addr(vec_bad_addr),
+      .xreg_valid (vec_xreg_valid),
+      .xreg_result(vec_xreg_result),
       .is_vector  (vec_is_vector),
       .vec_hold   (vec_hold),
       .vec_idle   (vec_idle),
@@ -742,6 +744,7 @@ module datapath
 
   always_comb begin
     if (is_muldiv_ex) result_ex = muldiv_result;
+    else if (vec_xreg_valid) result_ex = vec_xreg_result;
     else if (is_vset) result_ex = {{XLEN - 8{1'b0}}, vl_d};
     else if (csr_access_ex) result_ex = csr_rdata;
     else result_ex = alu_result;
